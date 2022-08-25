d60aa9bafca2219b6e47c5a70adf1d34f455157e
Ankit Bhusal <ankitbhusal95@gmail.com>
2022-08-25 07:30:14 +0545

    rrd added nested routes with passing id and using useParam hook

--- projects/react-router-dom/src/index.jsx
+++ projects/react-router-dom/src/index.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Contact from "./Contact";
 import About from "./About";
+import Post from "./Post";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,6 +14,9 @@ root.render(
         <Route path="/" element={<App />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
+        <Route path="/post" element={<Post />}>
+          <Route path="/post/:id" element={<Post />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
