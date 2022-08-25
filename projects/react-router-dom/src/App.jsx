664595875db593519fd1acbc1a40dcccb671dd35
Ankit Bhusal <ankitbhusal95@gmail.com>
2022-08-25 07:37:22 +0545

    added dynamic route

--- projects/react-router-dom/src/App.jsx
+++ projects/react-router-dom/src/App.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 const App = () => {
+  const [id, setId] = useState();
   return (
     <div>
       <div>This is a homepage</div> <br />
-      <Link to="/contact">Contact</Link> | <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link> |<Link to="/about">About</Link> |
+      <Link to="/post">Post</Link>
+      <br />
+      <br />
+      <input
+        type="text"
+        onChange={(e) => {
+          setId(e.target.value);
+        }}
+      />
+      <br />
+      <Link to={`/post/${id}`}>Post with value</Link>{" "}
     </div>
   );
 };
--- projects/react-router-dom/src/Post.jsx
+++ projects/react-router-dom/src/Post.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 const Post = () => {
   const { id } = useParams();
   return (
     <>
       <div>This is post page {id}</div>
+
+      <Link to="/">Home</Link>
     </>
   );
 };
