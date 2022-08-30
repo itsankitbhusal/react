50abc23aacfd705cec3c83b6cf1fef4aad3197f8
Ankit Bhusal <ankitbhusal95@gmail.com>
2022-08-30 11:18:57 +0545

    amde hamburger menu responsive

--- projects/chakra-app/src/App.js
+++ projects/chakra-app/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
-import Form from "./components/form/Form";
+
 import Nav from "./components/navbar/Nav";
 
 const App = () => {
   return (
     <>
       <Nav />
-
-      <Form />
     </>
   );
 };
--- projects/chakra-app/src/components/navbar/Nav.jsx
+++ projects/chakra-app/src/components/navbar/Nav.jsx
@@ -1,14 +1,15 @@
 import {
-  Text,
   Flex,
   Heading,
   HStack,
   Button,
   Box,
   useColorMode,
+  IconButton,
 } from "@chakra-ui/react";
 import React from "react";
 import { MdOutlineDarkMode } from "react-icons/md";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Nav = () => {
   const { toggleColorMode } = useColorMode();
@@ -27,14 +28,13 @@ const Nav = () => {
               Logo
             </Heading>
           </HStack>
-          <HStack gap={2}>
-            <Text _hover={{ color: "gray", cursor: "pointer" }}>Home</Text>
-            <Text _hover={{ color: "gray", cursor: "pointer" }}>Features</Text>
-            <Text _hover={{ color: "gray", cursor: "pointer" }}>About</Text>
-            <Text _hover={{ color: "gray", cursor: "pointer" }}>Contact</Text>
-          </HStack>
+          <HStack gap={2} display={["none", "none", "flex", "flex"]}>
+            <Button variant="ghost">Home</Button>
+            <Button variant="ghost">About</Button>
 
-          <HStack>
+            <Button variant="ghost">Contact</Button>
+          </HStack>
+          <HStack display={["none", "none", "flex", "flex"]}>
             <Box mx={4} _hover={{ color: "gray", cursor: "pointer" }}>
               <MdOutlineDarkMode
                 size="1.5rem"
@@ -44,6 +44,22 @@ const Nav = () => {
             </Box>
             <Button>Get Started</Button>
           </HStack>
+
+          <Flex align="center" display={["flex", "flex", "none", "none"]}>
+            <Box mx={4} _hover={{ color: "gray", cursor: "pointer" }}>
+              <MdOutlineDarkMode
+                size="1.5rem"
+                my={4}
+                onClick={toggleColorMode}
+              />
+            </Box>
+            <IconButton
+              aria-label="Open Menu"
+              size="lg"
+              mr={2}
+              icon={<GiHamburgerMenu />}
+            />
+          </Flex>
         </Flex>
       </Box>
     </>
