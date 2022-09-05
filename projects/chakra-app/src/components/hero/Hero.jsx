f4e4006d8ebc1d44951e83f4f46153ad267b2b95
Ankit Bhusal <ankitbhusal95@gmail.com>
2022-09-05 10:24:46 +0545

    fix hero section bugs

--- projects/chakra-app/src/components/hero/Hero.jsx
+++ projects/chakra-app/src/components/hero/Hero.jsx
@@ -18,27 +18,29 @@ const Hero = () => {
         <Flex
           height={["auto", "auto", "50vh", "75vh"]}
           align="center"
-          justify="center"
           gap={4}
+          flexWrap={["wrap", "wrap", "nowrap", "nowrap"]}
         >
-          <VStack w="50vw" display={["none", "none", "flex", "flex"]}>
-            <Img src={Image} width="full"></Img>
+          <VStack
+            w={["100vw", "100vw", "30vw", "30vw"]}
+            // display={["none", "none", "flex", "flex"]}
+          >
+            <Img src={Image} objectFit="cover"></Img>
           </VStack>
-          <VStack w="50vw" gap={4}>
+          <VStack
+            w={["100vw", "100vw", "70vw", "70vw"]}
+            marginTop={["-5rem", "-5rem", "0", "0"]}
+            gap={4}
+          >
             <Heading
+              // align={["center", "center", "auto", "auto"]}
+              align="center"
               as="h1"
               // size="4xl"
-              fontSize={"6em"}
-              alignSelf="flex-start"
-              noOfLines={2}
+              fontSize={["2em", "5em", "5em", "5em"]}
             >
               Hi! I Am{" "}
-              <Heading
-                as="h1"
-                // size="4xl"
-                fontSize={"1em"}
-                color="green.400"
-              >
+              <Heading as="h1" fontSize={"1.5em"} color="green.400">
                 Ankit Bhusal
               </Heading>
             </Heading>
@@ -46,7 +48,9 @@ const Hero = () => {
               //   alignSelf="flex-start"
               flexWrap={"wrap"}
               gap={4}
-              alignSelf={["center", "center", "flex-start", "flex-start"]}
+              // alignSelf={["center", "center", "flex-start", "flex-start"]}
+              alignSelf="center"
+              justifyContent="center"
             >
               <Button size="lg" gap={2}>
                 <FiPhone />
