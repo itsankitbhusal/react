891ec580ca32bdd9ec985962fc701ee4b7632cb2
Ankit Bhusal <ankitbhusal95@gmail.com>
2022-09-06 17:46:35 +0545

    added funcanality for scroll to top button

--- projects/chakra-app/src/components/SchrollToTop.jsx
+++ projects/chakra-app/src/components/SchrollToTop.jsx
@@ -1,13 +1,32 @@
 import { Box, Flex, IconButton } from "@chakra-ui/react";
 import React from "react";
 import { FaAngleUp } from "react-icons/fa";
+import { useState, useEffect } from "react";
 
 const SchrollToTop = () => {
+  const [showTopBtn, setShowTopBtn] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 400 ? setShowTopBtn(true) : setShowTopBtn(false);
+    });
+  }, []);
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   return (
     <Box pos="fixed" right="1rem" bottom="1rem">
-      <Flex>
-        <IconButton borderRadius="100%" icon={<FaAngleUp />} />
-      </Flex>
+      {showTopBtn && (
+        <Flex
+          onClick={() => {
+            goToTop();
+          }}
+        >
+          <IconButton borderRadius="100%" icon={<FaAngleUp />} />
+        </Flex>
+      )}
     </Box>
   );
 };
